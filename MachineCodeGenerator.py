0491f18e09c69dddc0ed5632301b0e81c2edd1db
Amirhossein <145652679+amir-2005@users.noreply.github.com>
2026-05-30 12:21:50 +0330

    Read the assembly code from .asm file instead of a fixed string

--- MachineCodeGenerator.py
+++ MachineCodeGenerator.py
@@ -8,28 +8,11 @@ reg = {
 # If you prefer a different behavior, change/remove this.
 reg['rd'] = reg['ra']  # map 'rd' -> x1 (ra)
 
-# Your assembly instructions as a string
-asm_code = """
-addi a1, zero, 20
-addi s0, zero, 0
-slli a1, a1, 2
-slt t0, s0, a1
-beq t0, zero, 13
-addi s1, s0, 4
-slt t1, s1, a1
-beq t1, zero, 9
-lw t2, 0(s0)
-lw t3, 0(s1)
-slt t4, t3, t2
-beq t4, zero, 3
-sw t3, 0(s0)
-sw t2, 0(s1)
-addi s1, s1, 4
-jal rd -9
-addi s0, s0, 4
-jal rd -14
-jal rd 0
-"""
+# Read assembly instructions from a .asm file
+asm_file = r"./Pipeline/program.asm"
+
+with open(asm_file, "r", encoding="utf-8") as f:
+    asm_code = f.read()
 
 # Parse the instructions into a list of tuples
 instructions = []
